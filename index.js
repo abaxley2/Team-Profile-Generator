17373c88862d11a4214db2e78568addaab921cd8
Austin Baxley <ausbaxley@gmail.com>
2021-08-12 17:39:05 -0400

    setup index functions

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
+// dependencies added
+const fs = require("fs");
+const inquirer = require("inquirer");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const path = require("path");
+const render = require("./dist/renderpage.js");
+const OUTPUT_DIR = path.resolve(__dirname, "dist");
+const outputPath = path.join(OUTPUT_DIR, "team-profile.html");
 
+// setup up arrays
+const employees = [];
+const employeeIDs = [];
+
+// functions for users
+function userPrompt() {
+  function makeManager() {}
+
+  function makeTeam() {}
+
+  function makeDirectory() {}
+}
+
+userPrompt();
